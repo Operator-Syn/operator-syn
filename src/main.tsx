532e1ce03b7f7d50b2f23c5322e345a6b536aa60
Operator-Eury <johnronanbeira100404@gmail.com>
2025-11-25 11:53:19 +0800

    Added a global head manager for the react application

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import { StrictMode, lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -6,9 +6,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
 import App from './App.tsx';
 
+const GlobalHeadManager = lazy(() => import('./components/globalHeadManager/GlobalHeadManager.tsx'));
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <Suspense fallback={null}>
+        <GlobalHeadManager />
+      </Suspense>
       <App />
     </BrowserRouter>
   </StrictMode>,
