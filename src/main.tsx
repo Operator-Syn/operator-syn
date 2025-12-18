f1b61537d0d8315c2703e1bee1f8f0ba9dec6a63
Operator-Eury <johnronanbeira100404@gmail.com>
2025-12-18 18:26:13 +0800

    Updated main.tsx to no longer use lazy import for GlobalHeadManager.

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
-import { StrictMode, lazy, Suspense } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
 import App from './App.tsx';
-
-const GlobalHeadManager = lazy(() => import('./components/globalHeadManager/GlobalHeadManager.tsx'));
+import GlobalHeadManager from './components/globalHeadManager/GlobalHeadManager.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Suspense fallback={null}>
         <GlobalHeadManager />
-      </Suspense>
       <App />
     </BrowserRouter>
   </StrictMode>,
