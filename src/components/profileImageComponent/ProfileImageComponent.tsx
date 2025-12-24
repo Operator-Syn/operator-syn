b6d6fe5d72187c805f42e30a0574d435ffa98ff8
Operator-Eury <johnronanbeira100404@gmail.com>
2025-12-24 18:00:17 +0800

    Updated ProfileImage with formatting and placeholders

--- src/components/profileImageComponent/ProfileImageComponent.tsx
+++ src/components/profileImageComponent/ProfileImageComponent.tsx
@@ -1,17 +1,36 @@
+import ProfileImagePlaceholder from "./ProfileImagePlaceholder";
+import AsyncImage from "../asyncImageLoader/AsyncImage";
+import "./ProfileImage.css";
+
 interface ProfileImageProps {
-    src: string;
-    className?: string; 
+    src?: string;
+    isLoading: boolean;
+    className?: string;
 }
 
-export default function ProfileImageComponent(props: ProfileImageProps) {
-    const { src, className = "" } = props;
+export default function ProfileImageComponent({ src, isLoading, className = "" }: ProfileImageProps) {
+    if (isLoading || !src) {
+        return (
+            <div className={`col-4 d-flex justify-content-center align-items-center ${className}`}>
+                <ProfileImagePlaceholder />
+            </div>
+        );
+    }
 
     return (
         <div className={`col-4 d-flex justify-content-center align-items-center ${className}`}>
-            <img
+            <AsyncImage
                 src={src}
                 alt="Profile"
-                className="img-thumbnail profileImage"
+                wrapperClassName="img-thumbnail rounded-circle profile-image-dimensions p-1"
+                loader={
+                    <div className="w-100 h-100 light-glass-blue-hue rounded-circle d-flex align-items-center justify-content-center">
+                         <span className="placeholder w-100 h-100 rounded-circle opacity-25"></span>
+                    </div>
+                }
+
+                // 3. IMAGE: The inner "Photo" content when ready
+                className="profileImage rounded-circle w-100 h-100"
             />
         </div>
     );
