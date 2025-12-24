8b530eafa61ef854cd349c99c3e52e51919518ee
Operator-Eury <johnronanbeira100404@gmail.com>
2025-12-24 18:03:27 +0800

    Update NavBar component and styling

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,6 +1,8 @@
 import { useState, useRef, useEffect, useCallback, type FC } from "react";
 import { NavLink } from "react-router-dom";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import "./NavBar.css";
 
 export interface NavLinkItem {
